bf688ef62e37132d3183eef29e2a1930d76bdb02
Alexis <alexis@ladydinde.com>
2023-03-23 22:33:59 +0100

    wip item

--- src/renderer/components/Item.vue
+++ src/renderer/components/Item.vue
@@ -3,23 +3,48 @@
 import { ref, computed, watch } from 'vue'
 
 const props = defineProps(['sound'])
+
 // type-based
 //check();
 </script>
 
 <template>
     <div class="row" >
-        <div><span class="number">{{ props.sound.id.toString().padStart(3,"0") }}</span> : {{ props.sound.text}}</div>  
-        <div></div>
+        <div class="status">•</div>
+        <div class="name"><span class="number">{{ props.sound.id.toString().padStart(3,"0") }}</span> : {{ props.sound.text}}</div>  
+        <div class="actions"><button disabled >↧</button><button disabled >↥</button><button disabled >⟲</button><button disabled >⨯</button></div>
     </div>
 </template>
 
 <style scoped lang="scss">
+button{
+    margin-left: 0.2rem;
+    padding: 0.2rem 0.4rem;
+}
+.status{
+    flex-direction: row;
+    align-items: center; 
+}
+.name{
+
+flex:1;
+}
+.actions{
+    justify-self: flex-end;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
 .number{
     color:#bf616a;
+    font-family: 'Courier New', Courier, monospace;
 }
 .row {
   //border-bottom: 1px solid #888;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    //justify-content: center;
     width:100%;
     text-align: left;
     color: #888;
