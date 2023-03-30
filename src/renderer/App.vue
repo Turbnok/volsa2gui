e18767215727824f925c0b5e29ad998f4a38dbaf
Turbnok <alexis@ladydinde.com>
2023-03-30 09:41:58 +0200

    showing version

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -2,10 +2,20 @@
 import Menu from "./components/Menu.vue"
 import List from "./components/List.vue"
 import Erro from "./components/Error.vue"
+import { version } from "../../package.json"
 </script>
 <template>
   <Menu />
   <List />
   <Erro />
+  <div class="version">v{{ version }}</div>
 </template>
-<style></style>
+<style>
+.version {
+  color : var(--nord3);
+  font-size: 0.6rem;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
+</style>
